dbe736586660c9d67267fbf0a0c33dfd0987a9f0
Marcel Nakamine <marcel.nakamine@gmail.com>
2017-05-23 12:19:47 -0300

    configuring the login, index and chat routes

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes');
 
 const port = 3000;
 
-app.get('*', (req,res) => {
-  res.send('Express response');
-});
+app.get('/', routes.index);
+app.get('/login', routes.login);
+app.post('/login', routes.loginProcess);
+app.get('/chat', routes.chat);
 
 app.listen(process.env.PORT || port);
 console.log(`App server running on port ${port}`);
