891cf915b57f1d76151af5516965c0db927bc0d4
Marcel Nakamine <marcel.nakamine@gmail.com>
2017-05-23 13:17:23 -0300

    adding page not found middleware

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const errorHandlers = require('./middleware/errorhandlers');
 
 const port = 3000;
 
+app.use(errorHandlers.notFound);
 app.get('/', routes.index);
 app.get('/login', routes.login);
 app.post('/login', routes.loginProcess);
